918b590821c2376fea46244fa4928447f7cba27e
GiverPlay007 <play.giver.007@gmail.com>
2020-12-23 10:16:40 -0300

    Checagem dos pinos de entrada para escrever o ângulo nos pinos de saída

--- main.ino
+++ main.ino
@@ -11,6 +11,12 @@
 Servo servoX;
 Servo servoY;
 
+int angleX = 0;
+int angleY = 0;
+
+int lastX = 0;
+int lastY = 0;
+
 void setup()
 {
     Serial.begin(9600);
@@ -29,5 +35,64 @@ void setup()
 
 void loop()
 {
+    checkAxisX();
+    checkAxisY();
+}
 
+void checkAxisX()
+{
+    if(digitalRead(xDir1))
+    {
+        ++angleX;
+
+        if(angleX > 180)
+        {
+            angleX = 180;
+        }
+    }
+
+    if(digitalRead(xDir2))
+    {
+        --angleX;
+
+        if(angleX < 0)
+        {
+            angleX = 0;
+        }
+    }
+
+    if(angleX != lastX)
+    {
+        servoX.write(angleX);
+        lastX = angleX;
+    }
+}
+
+void checkAxisY()
+{
+    if(digitalRead(yDir1))
+    {
+        ++angleY;
+
+        if(angleY > 180)
+        {
+            angleY = 180;
+        } 
+    }
+
+    if(digitalRead(yDir2))
+    {
+        --angleY;
+
+        if(angleY < 0)
+        {
+            angleY = 0;
+        }
+    }
+
+    if(angleY != lastY)
+    {
+        servoY.write(angleY);
+        lastY = angleY;
+    }
 }
