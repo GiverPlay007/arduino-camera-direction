9fc3a8b422319762e4b2067511562d7626ecfb8a
GiverPlay007 <play.giver.007@gmail.com>
2020-12-23 13:08:57 -0300

    Velocidade dos servos com potenciômetro

--- main.ino
+++ main.ino
@@ -8,6 +8,9 @@
 #define yDir1 2
 #define yDir2 3
 
+#define xSpeedInput A0
+#define ySpeedInput A1
+
 Servo servoX;
 Servo servoY;
 
@@ -17,6 +20,9 @@ int angleY = 0;
 int lastX = 0;
 int lastY = 0;
 
+int xSpeed = 1;
+int ySpeed = 1;
+
 void setup()
 {
     Serial.begin(9600);
@@ -25,6 +31,8 @@ void setup()
     pinMode(xDir2, INPUT);
     pinMode(yDir1, INPUT);
     pinMode(yDir2, INPUT);
+    pinMode(xSpeedInput, INPUT);
+    pinMode(ySpeedInput, INPUT);
 
     servoX.attach(axisX);
     servoY.attach(axisY);
@@ -35,15 +43,27 @@ void setup()
 
 void loop()
 {
+    checkSpeedX();
+    checkSpeedY();
     checkAxisX();
     checkAxisY();
+}
+
+void checkSpeedX()
+{
+    xSpeed = (int) (10 / 1024 * analogRead(xSpeedInput));
+}
+
+void checkSpeedY()
+{
+    ySpeed = (int) (10 / 1024 * analogRead(ySpeedInput));
 }
 
 void checkAxisX()
 {
     if(digitalRead(xDir1))
     {
-        ++angleX;
+        angleX += xSpeed;
 
         if(angleX > 180)
         {
@@ -53,7 +73,7 @@ void checkAxisX()
 
     if(digitalRead(xDir2))
     {
-        --angleX;
+        angleX -= xSpeed;
 
         if(angleX < 0)
         {
@@ -72,7 +92,7 @@ void checkAxisY()
 {
     if(digitalRead(yDir1))
     {
-        ++angleY;
+        angleY += ySpeed;
 
         if(angleY > 180)
         {
@@ -82,7 +102,7 @@ void checkAxisY()
 
     if(digitalRead(yDir2))
     {
-        --angleY;
+        angleY -= ySpeed;
 
         if(angleY < 0)
         {
